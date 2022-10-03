df1c31b8c5afcaa676676fe7a5b09c85030c8350
Alexandr Dolgov <dolgov659@gmail.com>
2022-10-03 20:23:28 +0300

    Added token security

--- BackEnd/src/main/java/app/seatsforplaces/security/jwt/JwtUtils.java
+++ BackEnd/src/main/java/app/seatsforplaces/security/jwt/JwtUtils.java
@@ -34,4 +34,26 @@ public class JwtUtils {
                 .compact();
     }
 
+    public String getPhoneNumberFromJwtToken(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e){
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        }catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
 }
