76369a5bcbd3d789fca2519ec76b5c368db5cf61
Alexandr Dolgov <dolgov659@gmail.com>
2022-10-03 16:22:34 +0300

    Added SignupRequest

---
+++ src/main/java/app/seatsforplaces/payload/request/SignupRequest.java
@@ -0,0 +1,29 @@
+package app.seatsforplaces.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+public class SignupRequest {
+
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotBlank
+    @Size(max = 11)
+    private String phonenumber;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private Set<String> role;
+
+}
